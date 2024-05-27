c492e27dc54a3c85a6dd4d6817c423b476d4acd6
GalacticFrost <steenkampjean@gmail.com>
2024-05-27 11:39:57 +0200

    B: Created main.sh; created .gitignore for all __pycache__ files/directories; Created textnode.py that contains TextNode class; defined main() in main.py and code to test TextNode class

---
+++ src/textnode.py
@@ -0,0 +1,17 @@
+class TextNode:
+
+    def __init__ (self, text, text_type, url):
+
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__ (self, second_textnode):
+
+        if self.text == second_textnode.text and self.text_type == second_textnode.text_type and self.url == second_textnode.url:
+            return True
+        return False
+        
+    def __repr__ (self):
+
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
